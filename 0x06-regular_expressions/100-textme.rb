8437e6a2c37532bc36fe910a021bacda5fb50d92
21Kgold <claudiarpa@gmail.com>
2022-06-22 20:29:14 -0700

    fix code fail twice

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -3,4 +3,4 @@ sender = ARGV[0].scan(/from:([a-zA-Z0-9_]+)/).join
 reciber = ARGV[0].scan(/to:([a-zA-Z0-9_]+)/).join
 flags = ARGV[0].scan(/flags:(\S^\]+)/).join
 array = [sender, reciber, flags]
-puts "join : #{array.join(", ")}"
+puts "join : #{array.join(",")}"
