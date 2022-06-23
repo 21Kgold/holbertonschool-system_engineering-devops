a864127737121b04ac53e34bb8acaddd112807ba
21Kgold <claudiarpa@gmail.com>
2022-06-22 21:02:17 -0700

    fix code fail twice

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
-data = ARGV[0].scan(/from:([a-zA-Z0-9_]+)|to:([a-zA-Z0-9_]+)|flags:([0-9:-]+)/)
+data = ARGV[0].scan(/from:(.[a-zA-Z0-9_]+)|to:(.[a-zA-Z0-9_]+)|flags:([0-9:-]+)/)
 array = [data[0].compact, data[1].compact, data[2].compact]
 puts array.join(',')
