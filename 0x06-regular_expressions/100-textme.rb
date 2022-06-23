c483da4c5444fc9c157918d27db392995791ccf5
21Kgold <claudiarpa@gmail.com>
2022-06-22 21:00:39 -0700

    fix code fail twice

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,6 +1,4 @@
 #!/usr/bin/env ruby
-sender = ARGV[0].scan(/from:([a-zA-Z0-9_]+)/).join
-reciber = ARGV[0].scan(/to:([a-zA-Z0-9_]+)/).join
-flags = ARGV[0].scan(/flags:(\S^\]+)/).join
-array = [sender, reciber, flags]
-puts "join : #{array.join(",")}"
+data = ARGV[0].scan(/from:([a-zA-Z0-9_]+)|to:([a-zA-Z0-9_]+)|flags:([0-9:-]+)/)
+array = [data[0].compact, data[1].compact, data[2].compact]
+puts array.join(',')
