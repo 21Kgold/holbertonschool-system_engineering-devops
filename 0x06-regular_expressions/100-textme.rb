bde719450907d304535c76014655ab170ea8d840
21Kgold <claudiarpa@gmail.com>
2022-06-22 20:27:20 -0700

    fix code fail

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
 sender = ARGV[0].scan(/from:([a-zA-Z0-9_]+)/).join
 reciber = ARGV[0].scan(/to:([a-zA-Z0-9_]+)/).join
-flags = ARGV[0].scan(/flags:(\S[^\]]+)/).join
-puts "#{sender}, #{reciber}, #{flags}"
+flags = ARGV[0].scan(/flags:(\S^\]+)/).join
+array = [sender, reciber, flags]
+puts "join : #{array.join(", ")}"
