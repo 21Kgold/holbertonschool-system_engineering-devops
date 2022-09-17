ca3e9614ca37edfa490e3a13c50cd9da569871e7
21Kgold <claudiarpa@gmail.com>
2022-09-17 00:00:09 -0700

    update

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Module that, using a REST API (https://jsonplaceholder.typicode.com/guide/),
+for a given employee ID (given as a second command line argument) export data
+in the CSV format. Format must be:
+"USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"
+File name must be: USER_ID.csv
+"""
+
+import requests
+import sys
+import csv
+
+if __name__ == "__main__":
+    URL = "https://jsonplaceholder.typicode.com/users/"
+    URL1 = URL + sys.argv[1]
+    URL2 = URL + sys.argv[1] + "/todos"
+    r1 = requests.get(URL1)
+    r2 = requests.get(URL2).json()
+    username = r1.json().get("username")
+    new = [dict(zip(['USER_ID', 'USERNAME', 'TASK_COMPLETED_STATUS',
+                    'TASK_TITLE'], [sys.argv[1], username,
+                    task_dictionary["completed"],
+                    task_dictionary["title"]])) for task_dictionary in r2]
+    filename = sys.argv[1] + ".csv"
+    data_file = open(filename, "w")
+    csv_writer = csv.writer(data_file, quoting=csv.QUOTE_ALL, quotechar='"')
+    for task_dictionary in new:
+        csv_writer.writerow(task_dictionary.values())
+    data_file.close()
