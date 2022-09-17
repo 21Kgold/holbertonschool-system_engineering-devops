b4e0b1cc2fa652daa7c1f503cd934f7f28c488b6
21Kgold <claudiarpa@gmail.com>
2022-09-17 01:35:11 -0700

    update

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -17,14 +17,13 @@ if __name__ == "__main__":
     URL2 = URL + sys.argv[1] + "/todos"
     r1 = requests.get(URL1)
     r2 = requests.get(URL2).json()
-    username = r1.json().get("username")
-    new = [dict(zip(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS",
-                    "TASK_TITLE"], [int(sys.argv[1]), username,
+    user_name = r1.json().get("username")
+    new = [dict(zip(["id", "username", "completed",
+                    "title"], [int(sys.argv[1]), user_name,
                     task_dictionary["completed"],
                     task_dictionary["title"]])) for task_dictionary in r2]
     filename = sys.argv[1] + ".csv"
     with open(filename, "w") as my_file:
-        csv_writer = csv.writer(my_file, quoting=csv.QUOTE_ALL, quotechar='"',
-                                lineterminator='\n')
+        csv_writer = csv.writer(my_file, quoting=csv.QUOTE_ALL, quotechar='"')
         for task_dictionary in new:
             csv_writer.writerow(task_dictionary.values())
